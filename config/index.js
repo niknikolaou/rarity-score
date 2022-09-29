a3a89169c109600311259a5cbaf83e9f501792af
TestingIt21 <101402343+TestingIt21@users.noreply.github.com>
2022-09-29 12:08:02 +0100

    fbg

--- config/index.js
+++ config/index.js
@@ -1,15 +1,15 @@
 export const config = {
-  env: "prod",
+  env: "local",
   LOCAL_API_URL: "localhost:3000",
-  API_URL: "odp-rarity.vercel.app",
-  COLLECTION_NAME: "OneDayPunks",
-  COLLECTION_TITLE: "One Day Punks",
-  COLLECTION_DESCRIPTION: '10k "One Day I\'ll Be A Punk"-punks.',
-  COLLECTION_IMG_LINK: "https://punkscape.xyz/og.png",
-  CONTRACT: {
-    // if you're not sure, set CONTRACT: {}, will remove opensea link
-    CHAIN: "ethereum", //possible values of ethereum, matic (polygin), klatyn, solana
-    ADDRESS: "0x5537d90a4a2dc9d9b37bab49b490cf67d4c54e91", //note: for solana these values change, so currently not supported
-  },
+  API_URL: "",
+  COLLECTION_NAME: "Fireball Genisis Collection",
+  COLLECTION_TITLE: "Fireball Genisis",
+  COLLECTION_DESCRIPTION: "Fireball Genisis Collection",
+  COLLECTION_IMG_LINK: "",
+  CONTRACT: {},
+  // if you're not sure, set CONTRACT: {}, will remove opensea link
+  // CHAIN: "", //possible values of ethereum, matic (polygin), klatyn, solana
+  // ADDRESS: "", //note: for solana these values change, so currently not supported
+
   WEIGHTS: {},
 };
