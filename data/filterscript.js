a7f404069100552ddaaadb4a2e041aefcea05b52
niknikolaou <nik.nikolaou@gmail.com>
2023-03-19 21:47:12 +0200

    update

--- data/filterscript.js
+++ data/filterscript.js
@@ -3,8 +3,8 @@ const collection = require("./collection.json");
  * script for running any filters on collection
  */
 
-for (let i = 1; i < collection.length; i++) {
-  for (let j = 1; j < collection[i]["attributes"].length; j++) {
+for (let i = 0; i < collection.length; i++) {
+  for (let j = 0; j < collection[i]["attributes"].length; j++) {
     let attribute = collection[i]["attributes"][j];
     console.log(attribute);
   }
